4c28b177ae7fe321d744df960dfeb56941e7d2a0
Norgoci Nici Nici <norgoci.nici.nici@gmail.com>
2018-05-11 13:44:29 +0200

    code cleanups

--- example.js
+++ example.js
@@ -4,5 +4,5 @@ let account = "0x00a329c0648769a73afac7f9381e08fb43dbea72";
 // default paritiy port
 let nodeURL = "http://localhost:8545";
 
-let toolbox = require('./ParityToolbox');
+var toolbox = require('parity-toolbox');
 toolbox.deploy(contractFile, account, nodeURL);
