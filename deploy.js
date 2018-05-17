fd1b36291a8f9e065b5072b4ff4c7d88ae388c80
Norgoci Nici Nici <norgoci.nici.nici@gmail.com>
2018-05-17 21:24:02 +0200

    better example

--- deploy.js
+++ deploy.js
@@ -1,8 +1,7 @@
 let contractFile = "./test/contract.sol";
 // this is the default parity developement account (not for production)
 let account = "0x00a329c0648769a73afac7f9381e08fb43dbea72";
-// default paritiy port
 let nodeURL = "http://localhost:8545";
+var toolbox = require('./index.js');
 
-var toolbox = require('parity-toolbox');
 toolbox.deploy(contractFile, account, nodeURL);
