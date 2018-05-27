4e9a7286157982ec0a5d0131193f322868541d9f
Norgoci Nici Nici <norgoci.nici.nici@gmail.com>
2018-05-27 13:19:12 +0200

    the tests are moved in a separate project

--- start-partity.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#set -xv
-
-# the "0x00a329c0648769a73afac7f9381e08fb43dbea72" is the develop user
-parity --chain dev --base-path=./.parity --unlock 0x00a329c0648769a73afac7f9381e08fb43dbea72 --password ./password
--- test/contract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.21;
-
-
-contract Solution {
-
-    function getSolution() public pure returns (uint) {
-        return 42;
-    }
-}
