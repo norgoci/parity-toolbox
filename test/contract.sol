b75ad978cdf549de873416b8a110155061fbfde2
Norgoci Nici Nici <norgoci.nici.nici@gmail.com>
2018-05-27 13:16:56 +0200

    the contract is more simplistic

--- test/contract.sol
+++ test/contract.sol
@@ -1,21 +1,9 @@
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+pragma solidity ^0.4.21;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
 
-  function Migrations() {
-    owner = msg.sender;
-  }
+contract Solution {
 
-  function setCompleted(uint completed) restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    function getSolution() public pure returns (uint) {
+        return 42;
+    }
 }
